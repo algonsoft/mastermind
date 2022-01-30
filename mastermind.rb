f5077b91ad3b9d79f549ffaac9e7caf354d980a1
algonsoft <smtalgonsoft@gmail.com>
2022-01-30 16:31:07 +1030

    Human guesses code implemented

--- mastermind.rb
+++ mastermind.rb
@@ -5,6 +5,7 @@
 # Duplicate pegs are allowed in the secret code but blanks are not.
 class Mastermind
   def initialize
+    @legit_guess = 0
     @game_won = 0
     @move = 1
     @colors = %w[r o y g b p]
@@ -17,28 +18,58 @@ class Mastermind
       rand_num = rand(@colors.length)
       @code.append(@colors[rand_num])
     end
-    puts @code
   end
 
   def guess
     puts 'Enter a 4 Letter Combo Consisting of the letters (r o y g b p)'
-    guess = gets.chomp.split(//)
-    if guess.length == 4 && (guess - @colors).empty?
-      puts 'Legitimate Guess'
+    @guess = gets.chomp.split(//)
+    if @guess.length == 4 && (@guess - @colors).empty?
+      puts "Move #{@move}"
       @move += 1
+      @legit_guess = 1
     else
-      puts 'Incorrect Guess Try Again'
+      puts 'Illegal Guess!'
+      guess
+    end
+  end
+
+  def legit_guess
+    @legit_guess
+  end
+
+  def move
+    @move
+  end
+
+  def game_won
+    @game_won
+  end
+
+  def evaluate_guess
+    @guess.each_with_index do |value, index|
+      if value == @code[index]
+        puts "Guess #{value} at position #{index} is correct!"
+      elsif @code.include?(value)
+        puts "Guess #{value} is present but not at location #{index}!"
+      else
+        puts "Guess #{value} is not present in the array!"
+      end
     end
   end
 
   def check_victory
-    puts '...'
+    return unless @guess == @code
+
+    puts 'You have cracked the code!'
+    @game_won = 1
   end
 end
 
 game = Mastermind.new
 game.generate_code
 
-while @game_won.zero?
-game.guess
+while game.game_won.zero? && game.move <= 12
+  game.guess
+  game.evaluate_guess
+  game.check_victory
 end
